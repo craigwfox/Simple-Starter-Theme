5e1e753cb9b45651df486a674dfd6394bb745c1a
craigwfox <trg@Development-Powerhouse.local>
2014-01-07 13:44:15 -0600

    added to imageoptim

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,53 +1,63 @@
 module.exports = function(grunt) {
-	// Do grunt-related things in here
+  // Do grunt-related things in here
 
-	grunt.initConfig({
+  grunt.initConfig({
 
-	// Watch
-		watch: {
-			css: {
-				files: 'assets/*.scss',
-				tasks: ['sass'],
-				options: {
-					livereload: true,
-				},
-			},
+  // Watch
+    watch: {
+      css: {
+        files: 'assets/*.scss',
+        tasks: ['sass'],
+        options: {
+          livereload: true,
+        },
+      },
 
-			scripts: {
-				files: ['assets/jsbin/*.js'],
-				tasks: ['uglify'],
-				options: {
-					spawn: false,
-				},
-			},
+      scripts: {
+        files: ['assets/jsbin/*.js'],
+        tasks: ['uglify'],
+        options: {
+          spawn: false,
+        },
+      },
 
-		},
+    },
 
-		// CSS
-		sass: {
-			dist: {
-				options: {
-					style: 'compressed'
-				},
-				files: {
-					'assets/css/screen.css': 'assets/sass/screen.scss',
-				}
-			},
-		},
+  // CSS
+    sass: {
+      dist: {
+        options: {
+          outputStyle: 'compressed'
+        },
+        files: {
+          'assets/css/screen.css': 'assets/sass/screen.scss',
+        }
+      },
+    },
 
-				// JS
-		uglify: {
-			my_target: {
-				files: {
-					'assets/js/site.min.js': ['assets/jsbin/site.js']
-				}
-			}
-		}
-	});
+  // JS
+    uglify: {
+      my_target: {
+        files: {
+          'assets/js/site.min.js': ['assets/jsbin/site.js']
+        }
+      }
+    }
 
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-sass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default',['watch']);
-	grunt.loadNpmTasks('grunt-notify');
+  // Images
+    imageoptim: {
+      imageOp: {
+        src: ['assets/imgs']
+      }
+    },
+
+  });
+
+
+  grunt.loadNpmTasks('grunt-imageoptim');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default',['watch']);
+  grunt.loadNpmTasks('grunt-notify');
 };
