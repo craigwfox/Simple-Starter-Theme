6209a3d7faabbd1b1179063d3c715f71b4148053
craigwfox <craig@craigwfox.com>
2014-06-19 08:16:10 -0500

    Bug fixes in home slider call and in _mixinx.scss

--- page_home.php
+++ page_home.php
@@ -8,14 +8,29 @@ Template Name: Home Page
       <div class="slider main-slider">
         <?php
           $mainSliderArgs = array(
-            'post-type' => 'scpt_banners',
-            'scpt_tax_banners' => ,
+            'post-type' => 'sspt_banners',
+            'sspt_tax_banners' => ,
             'showposts' => 10,
           );
           $mainSliderQuery = new WP_Query($mainSliderArgs);
           while ($mainSliderQuery->have_posts()) : $mainSliderQuery->the_post(); {
         ?>
         <?php } endwhile; wp_reset_postdata(); ?>
-      </div>
-    </div>
+      </div><!-- Main Slider -->
+
+      <div class="home-content grid-row">
+        <section class="intro grid-2-3">
+          <?php if (have_posts()) : ?>
+          <?php while (have_posts()) : the_post(); ?>
+              <?php the_content('Read the rest of this entry &raquo;'); ?>
+          <?php endwhile; ?>
+          <?php else : ?>
+            <h2>Not Found</h2>
+            <p>Sorry, but you are looking for something that isn't here.</p>
+          <?php endif; ?>
+        </section>
+        <aside class="home-sidebar grid-1-3"></aside>
+      </div><!-- Home Content -->
+
+    </div><!-- Main -->
 <?php get_footer(); ?>
