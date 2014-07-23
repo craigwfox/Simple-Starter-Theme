6bde26c63ff5acfe6f5a199bdf7b01d9a8543a15
Craig Fox <craig@therichlandgroup.com>
2014-07-23 14:26:18 -0500

    swapped back from gulp to grunt

---
+++ Gruntfile.js
@@ -0,0 +1,75 @@
+module.exports = function(grunt) {
+  // Do grunt-related things in here
+
+  grunt.initConfig({
+
+  // Watch
+    watch: {
+      css: {
+        files: 'assets-build/sass/**/*.scss',
+        tasks: ['sass', 'autoprefixer'],
+        options: {
+          livereload: true,
+        },
+      },
+
+      scripts: {
+        files: ['assets-build/js/**/*.js'],
+        tasks: ['uglify'],
+        options: {
+          spawn: false,
+        },
+      },
+
+    },
+
+  // CSS
+    sass: {
+      dist: {
+        options: {
+          outputStyle: 'compressed'
+        },
+        files: {
+          'assets/css/screen.css': 'assets-build/sass/screen.scss',
+        }
+      },
+    },
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 version', 'ie 8', 'ie 9']
+      },single_file: {
+      options: {
+        // Target-specific options go here.
+      },
+        src: 'assets/css/screen.css',
+        dest: 'assets/css/screen.min.css'
+      },
+    },
+
+  // JS
+    uglify: {
+      my_target: {
+        files: {
+          'assets/js/main.min.js': ['assets-build/js/**/*.js', '!assets-build/js/home/**/*.js']
+        }
+      }
+    },
+
+  // Images
+    imageoptim: {
+      imageOp: {
+        src: ['assets/images']
+      }
+    },
+
+  });
+
+
+  grunt.loadNpmTasks('grunt-imageoptim');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default',['watch']);
+  grunt.loadNpmTasks('grunt-notify');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+};
