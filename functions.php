feced0369f58b1909500181b8c8fceb2428a39a8
Craig Fox <craig@craigwfox.com>
2015-03-30 11:09:19 -0500

    removed functions taking out classes on menus

--- functions.php
+++ functions.php
@@ -127,12 +127,6 @@ if ( ! isset( $content_width ) ) {
     }
 
   /* --- Clean Up menus --- */
-    add_filter('nav_menu_css_class', 'my_css_attributes_filter', 100, 1);
-    add_filter('nav_menu_item_id', 'my_css_attributes_filter', 100, 1);
-    add_filter('page_css_class', 'my_css_attributes_filter', 100, 1);
-    function my_css_attributes_filter($var) {
-      return is_array($var) ? array() : '';
-    }
     // remove ul wp_nav_menu
     function remove_ul ( $menu ){
         return preg_replace( array( '#^<ul[^>]*>#', '#</ul>$#' ), '', $menu );
